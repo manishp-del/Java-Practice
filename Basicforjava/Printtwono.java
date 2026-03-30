8d7fdac753952c78236cc8424bae0c55246fe0fa
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-03-31 00:54:36 +0530

    Add Printtwono.java to display two numbers

---
+++ Basicforjava/Printtwono.java
@@ -0,0 +1,10 @@
+package Basicforjava;
+public class Printtwono {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("The first number is: " + num1);
+        System.out.println("The second number is: " + num2);
+    }
+    
+}
