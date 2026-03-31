e7d560b0c2e7b1b521b9dc1753d24c2c11b89e3d
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-03-31 22:41:40 +0530

    Add sumofab.java to calculate and display the sum of two integers

---
+++ Basicforjava/sumofab.java
@@ -0,0 +1,14 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class sumofab {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+    
+}
