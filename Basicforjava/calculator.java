70d2bb2f9ca914d2ee39647b5adb2ac9d8b8a55a
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-03-31 20:30:22 +0530

    Add calculator.java to perform basic arithmetic operations

---
+++ Basicforjava/calculator.java
@@ -0,0 +1,53 @@
+package Basicforjava;
+
+
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch(op) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if(num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result = " + result);
+        sc.close();
+    }
+}
