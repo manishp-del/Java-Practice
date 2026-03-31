e8ea32061aa658b9a316f78b23681dd07684a24d
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-03-31 12:19:07 +0530

    Add subtwono.java to subtract two numbers and display the result

---
+++ Basicforjava/subtwono.java
@@ -0,0 +1,14 @@
+
+// This is a simple Java program that subtracts two numbers and prints the result.
+package Basicforjava;
+
+public class subtwono {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sub = a - b;
+        System.out.println(sub);
+    }
+
+    
+}
