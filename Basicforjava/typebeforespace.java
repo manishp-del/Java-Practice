fac34091fe9a8c630d1e27d1284afc486542042c
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-03-31 21:30:26 +0530

    Add typebeforespace.java and typefullline.java for input handling examples

---
+++ Basicforjava/typebeforespace.java
@@ -0,0 +1,12 @@
+package Basicforjava;
+
+import java.util.Scanner;
+
+public class typebeforespace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+    }
+    
+}
---
+++ Basicforjava/typefullline.java
@@ -0,0 +1,12 @@
+package Basicforjava;
+
+import java.util.Scanner;
+
+public class typefullline {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
+
