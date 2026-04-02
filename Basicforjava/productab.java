c1d54a556d70310739e36e9ec2b40add6ecff65a
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-02 10:55:16 +0530

    Add productab.java to calculate and display the product of two integers

---
+++ Basicforjava/productab.java
@@ -0,0 +1,13 @@
+package Basicforjava;
+
+import java.util.Scanner;
+public class productab {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
+    }
+    
+}
