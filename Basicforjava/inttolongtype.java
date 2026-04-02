0879aa25546264195756a22f98d0a5d85d65b67c
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-02 13:22:48 +0530

    Add inttolongtype.java to demonstrate variable assignment and output in Java

---
+++ Basicforjava/inttolongtype.java
@@ -0,0 +1,10 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class inttolongtype {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = a;
+        System.out.println(b);
+    }
+}
