d77f700d00d959ac68a6dfe88bc979795ffef1e4
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-02 10:57:08 +0530

    Add areaofcircle.java to calculate and display the area of a circle based on user input

---
+++ Basicforjava/areaofcircle.java
@@ -0,0 +1,12 @@
+package Basicforjava;
+import java.util.Scanner;
+public class areaofcircle {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        float rad = SC.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+
+    }
+    
+}
