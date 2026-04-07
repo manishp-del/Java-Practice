1d1846c70d2ae33ff6b605fc5994a6327ef15856
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-07 09:42:24 +0530

    Add modulooperator.java to demonstrate the use of the modulo operator in Java

---
+++ Basicforjava/modulooperator.java
@@ -0,0 +1,11 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class modulooperator {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("modulo(remainder) = " + (a%b));
+    }
+}
