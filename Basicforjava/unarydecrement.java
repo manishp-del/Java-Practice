2e3006d584521d1fd059af7d180006901ba66276
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-07 20:19:24 +0530

    Add unarydecrement.java and unaryincrement.java to demonstrate the use of unary decrement and increment operators in Java

---
+++ Basicforjava/unarydecrement.java
@@ -0,0 +1,17 @@
+package Basicforjava;
+import java.util.Scanner;
+public class unarydecrement {
+    public static void main(String[] args) {
+       //predecrement
+        /*int a = 10;
+        int b = --a;
+        System.out.println(a);
+        System.out.println(b);/* */
+        //postdecrement
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b); 
+    }
+    
+}
---
+++ Basicforjava/unaryincrement.java
@@ -0,0 +1,17 @@
+package Basicforjava;
+import java.util.Scanner;
+public class unaryincrement {
+    public static void main(String[] args) {
+       //preincrement
+        /*int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);*/
+        //postincrement
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b); 
+    }
+    
+}
