6cd7cc627e74d8098d0c67beb760eb07e6210908
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-07 22:40:49 +0530

    Add calculator2.java to implement a basic calculator using arithmetic operations in Java

---
+++ Basicforjava/calculator2.java
@@ -0,0 +1,23 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class calculator2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char operator = sc.next().charAt(0);
+
+        switch(operator) {
+            case '+' : System.out.println(a+b);
+            break;
+            case '-' : System.out.println(a-b);
+            break;
+            case '*' : System.out.println(a*b);
+            break;
+            case '/' : System.out.println(a/b);
+            break;
+        }
+    }
+    
+}
