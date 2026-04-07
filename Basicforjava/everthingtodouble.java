2c236fc136e403a9f217167b606bf9569fb69d61
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-07 08:27:35 +0530

    Add everthingtodouble.java to demonstrate addition of various numeric types resulting in a double Add typepromoex.java to illustrate type promotion and casting in arithmetic operations

---
+++ Basicforjava/everthingtodouble.java
@@ -0,0 +1,17 @@
+package Basicforjava;
+
+
+import java.util.Scanner;
+
+public class everthingtodouble {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d =30;
+        double answer = a+b+c+d;
+        System.out.println(answer);
+
+    }
+    
+}
---
+++ Basicforjava/typepromoex.java
@@ -0,0 +1,12 @@
+package Basicforjava;
+import java.util.Scanner;
+public class typepromoex {
+    public static void main(String[] args) {
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte d = (byte)(a+b+c);
+        System.out.println(d);
+    }
+    
+}
