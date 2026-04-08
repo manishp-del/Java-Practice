1a115fb079c4a985e8f3502009c09491e57eb9bf
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-08 21:14:37 +0530

    Add leapyearornot.java to determine if a given year is a leap year

---
+++ Basicforjava/leapyearornot.java
@@ -0,0 +1,23 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class leapyearornot {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year :");
+        int year = sc.nextInt();
+        if((year%4==0 && year%100!=0) || (year%400==0))
+                    System.out.println("The year is a leap year");
+                
+                
+                else{
+                    System.out.println("The year is not a leap year");
+
+                }
+
+            
+        }
+    }
+
+    
+
