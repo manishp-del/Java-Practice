9b5c70a222a3d129ada7aebb619b9b567d096bbb
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-08 11:02:45 +0530

    Add posinega.java to determine if a number is positive, negative, or zero

---
+++ Basicforjava/posinega.java
@@ -0,0 +1,21 @@
+package Basicforjava;
+import java.util.Scanner;
+public class posinega {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int a = sc.nextInt();
+        if(a>0) {
+            System.out.println("The number is positive");
+
+        }
+        else if(a<0){
+            System.out.println("The number is negative:");
+
+        }
+        else{
+            System.out.println("The number is zero: ");
+        }
+    }
+    
+}
