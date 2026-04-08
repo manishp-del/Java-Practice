a95064e27066456fc8e116c3859e1e5d85ef4cfa
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-08 20:45:03 +0530

    Add q6.java to demonstrate conditional operations and boolean evaluation in Java

---
+++ Basicforjava/q6.java
@@ -0,0 +1,14 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class q6 {
+    public static void main(String[] args) {
+        int a = 63, b = 36;
+        boolean x = (a<b) ? true : false;
+        int y = (a>b) ? a:b;
+        System.out.println(x);
+        System.out.println(y);
+
+    }}
+
+    
