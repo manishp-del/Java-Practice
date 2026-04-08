b006290735f9c5532ce8d5851ab5b3b4ec3301cf
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-08 20:08:09 +0530

    Add weekname.java to display the name of the week based on user input

---
+++ Basicforjava/weekname.java
@@ -0,0 +1,37 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class weekname {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter week number(1-7): ");
+        int week = sc.nextInt();
+
+        switch(week) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                    System.out.println("Invalid input! please enter week nuber between (1-7). ");
+        }
+    
+}
+}
