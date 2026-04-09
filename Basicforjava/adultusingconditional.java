3919921bb71ba5487cb80558620c0aaefc01f3a4
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-09 09:54:39 +0530

    Add adultusingconditional.java to determine if a user is an adult or teenager based on age input

---
+++ Basicforjava/adultusingconditional.java
@@ -0,0 +1,22 @@
+package Basicforjava;
+import java.util.Scanner;
+
+public class adultusingconditional {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age :");
+        int age =  sc.nextInt();
+        if(age>=18){
+            System.out.println("you r an adult ");
+        }
+        if(age>=13 && age<18){
+            System.out.println("you r teenager");
+
+        }
+        else{
+            System.out.println("you r teenager");
+        }
+    }
+    
+}
