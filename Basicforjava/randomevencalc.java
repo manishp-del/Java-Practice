55f2a1a380b05386ad4caf39a43a881f15ceb44d
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-12 23:53:42 +0530

    Add randomevencalc.java to print even numbers between 10 and 100

---
+++ Basicforjava/randomevencalc.java
@@ -0,0 +1,14 @@
+package Basicforjava;
+import java.util.Scanner;
+public class randomevencalc {
+
+    public static void main(String[] args){
+        for(int i=10;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+
+            }
+        }
+    }
+    
+}
