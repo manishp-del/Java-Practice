ebc1a9e394eaaf0a81cb6ff7be7f77a8b930a2cd
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-13 21:08:26 +0530

    Add printpatternABC.java to print a pattern of letters using nested loops

---
+++ Basicforjava/printpatternABC.java
@@ -0,0 +1,17 @@
+package Basicforjava;
+
+
+public class printpatternABC {
+    public static void main(String[] args){
+        char ch = "A"
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.println(ch);
+                ch++;
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
