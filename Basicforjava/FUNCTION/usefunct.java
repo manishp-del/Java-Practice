fbe5033498a3446fdaaf3e1263bb31649bdaf212
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-13 21:15:59 +0530

    Add usefunct.java and usefunct2.java for basic function demonstration

---
+++ Basicforjava/FUNCTION/usefunct.java
@@ -0,0 +1,9 @@
+*package Basicforjava.FUNCTION;
+
+public class usefunct {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    
+}
---
+++ Basicforjava/FUNCTION/usefunct2.java
@@ -0,0 +1,10 @@
+package Basicforjava.FUNCTION;
+
+public class usefunct2 {
+    public static int main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        return 0;
+    }
+}
