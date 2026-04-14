571230c512e61349be061603a1889abfa8f17b44
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-14 10:35:17 +0530

    Add sum.java and sumof.java for basic sum calculation demonstration

---
+++ Basicforjava/FUNCTION/sum.java
@@ -0,0 +1,21 @@
+package Basicforjava.Function;
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args){
+        sum();
+
+    }
+    public static void sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.println("Enter b :");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("The sum is : "  + sum);
+
+
+    }
+    
+    
+}
