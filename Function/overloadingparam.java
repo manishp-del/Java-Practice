ec397d164141b60fa1e2471802e10e2caa75194f
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-04-17 23:14:56 +0530

    Add overloadingparam.java for method overloading demonstration

---
+++ Function/overloadingparam.java
@@ -0,0 +1,31 @@
+package Function;
+import java.util.Scanner;
+
+public class overloadingparam {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+
+    }
+
+    
+
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a : ");
+    int a = sc.nextInt();
+    System.out.println("Enter b : ");
+    int b = sc.nextInt();
+    System.out.println("Enter c : ");
+    int c = sc.nextInt();
+    int result1 = sum(a, b);
+    System.out.println("result1 is :" + result1);
+    int result2 = sum(a, b, c);
+    System.out.println("result2 is : "+  result2);
+    
+
+   
+}
+}
