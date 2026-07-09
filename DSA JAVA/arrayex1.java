b94bae147a478002026f7ab8fa5aa9571534b30d
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-09 22:48:21 +0530

    Add arrayex1.java for inputting and displaying student marks

---
+++ DSA JAVA/arrayex1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class arrayex1 {
+    public static void main(String[] args){
+        int[] marks = new int[100];
+        Scanner sc = new Scanner(System.in);
+        marks[0]= sc.nextInt();
+        marks[1]= sc.nextInt();
+        marks[2]= sc.nextInt();
+
+        System.out.println("marks of student 1 is "+marks[0]);
+        System.out.println("marks of student 2 is "+marks[1]);
+        System.out.println("marks of student 3 is "+marks[2]);
+        sc.close();
+    }
+    
+}
