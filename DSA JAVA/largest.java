4d340a1fa58712186388f8afba9a258cd4b0b79a
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-11 12:01:48 +0530

    Add largest.java for finding the largest and smallest values in an array

---
+++ DSA JAVA/largest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class largest{
+    public static int largest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for(int i =0; i<numbers.length; i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest>numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("smallest value is:" + smallest);
+        return largest;
+
+    }
+    public static void main(String args[]) {
+        int numbers[] ={1, 2, 3, 4, 5};
+        System.out.println("largest value is :" + largest(numbers));
+    }
+
+
+}
