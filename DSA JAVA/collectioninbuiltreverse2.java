fce35f3b7a7fb8c0e5dad2789a17cb24cdf8ab05
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-17 10:42:41 +0530

    Add collectioninbuiltreverse.java and collectioninbuiltreverse2.java for sorting arrays in reverse order; update inbuiltsort2.java for code consistency

--- DSA JAVA/collectioninbuiltreverse2.java
+++ DSA JAVA/collectioninbuiltreverse2.java
@@ -1,16 +1,18 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
-public class inbuiltsort2 {
+import java.util.Collections;
+public class collectioninbuiltreverse2  {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array:");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        Integer arr[] = new Integer[n];
         System.out.println("Enter the array elements: ");
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr, 0, 3);
+        Arrays.sort(arr, 0, 3, java.util.Collections.reverseOrder());
         System.out.println("Sorted array:");
         for(int i=0; i<n; i++){
             System.out.print(arr[i]  + " ");
