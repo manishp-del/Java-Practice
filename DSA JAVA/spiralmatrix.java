760855df6e12b37c4136aec2f45eb8910105801d
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-23 23:41:39 +0530

    Add matrix2d.java for creating and displaying a 2D matrix from user input Add matrixsearch2d.java for searching a key in a 2D matrix and displaying its position Add spiralmatrix.java for printing a 2D matrix in spiral order

---
+++ DSA JAVA/spiralmatrix.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class spiralmatrix {
+
+    public static void printSpiral(int matrix[][]) {
+
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+
+            // Top row
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            // Right column
+            for (int i = startRow + 1; i <= endRow; i++) {
+                if (startRow == endRow)
+                    break;
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // Bottom row
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startCol == endCol)
+                    break;
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // Left column
+            for (int i = endRow - 1; i >= startRow + 1; i--) {
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int matrix[][] = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        printSpiral(matrix);
+    }
+}
