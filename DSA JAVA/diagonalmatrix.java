795104f33f8b073584488f27db3bcc6623c6e40a
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-24 15:44:56 +0530

    Remove unnecessary initialization of the matrix in diagonalmatrix.java

--- DSA JAVA/diagonalmatrix.java
+++ DSA JAVA/diagonalmatrix.java
@@ -20,8 +20,6 @@ public class diagonalmatrix {
     }
 
     public static void main(String args[]) {
-
-        int matrix[][] = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
             {9, 10, 11, 12},
