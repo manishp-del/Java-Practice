2e62e6e7187facd9f08d2f6e25aad5aa6766652b
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-25 22:18:37 +0530

    Add concatanationfunction.java to demonstrate string concatenation in Java

---
+++ DSA JAVA/concatanationfunction.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class concatanationfunction {
+    public static void printletters(String str){
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        String firstname = "manish";
+        String lastname = "Prasad";
+        String fullname = firstname + " " + lastname;
+        printletters(fullname);
+
+    }
+    
+}
