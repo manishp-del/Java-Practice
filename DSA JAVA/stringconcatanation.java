38d74e607503dc3c2413b87440ccab804b575b94
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-25 22:00:41 +0530

    Add stringconcatanation.java for demonstrating string concatenation in Java

---
+++ DSA JAVA/stringconcatanation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class stringconcatanation {
+    public static void main(String args[]){
+
+    
+       String firstname = "Manish";
+       String lastname = "Prasad";
+       String fullname = firstname + " " + lastname;
+       System.out.println(fullname + "is this");
+    
+}
+}
