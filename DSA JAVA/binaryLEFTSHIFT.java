56b515c7fae3e9da47eadf373742e6acc4c3ee0c
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-07-28 18:26:23 +0530

    Add binary operation examples: AND, OR, XOR, and LEFT SHIFT

---
+++ DSA JAVA/binaryLEFTSHIFT.java
@@ -0,0 +1,9 @@
+public class binaryLEFTSHIFT {
+    public static void main(String args[]){
+        System.out.println(5 << 2);
+    }
+    
+}
+
+    
+
---
+++ DSA JAVA/binaryOR.java
@@ -0,0 +1,9 @@
+public class binaryOR {
+    public static void main(String args[]){
+        System.out.println(5 | 6);
+    }
+    
+}
+
+    
+
