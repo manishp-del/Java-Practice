9f34cd88dd05f8d632b19d065f80fecc108bd505
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-08-09 18:44:25 +0530

    Add MERGESORT.java to demonstrate recursion for sorting an array using the merge sort algorithm

---
+++ DSA JAVA/RECURSION/MERGESORT.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class MERGESORT {
+    public static void divideconquer(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] +" ");
+
+        }
+        System.out.println();
+
+
+    }
+    public static void mergesort(int arr[], int si, int ei){
+        //base case
+        if(si>=ei){
+            return;
+        }
+        //kaam
+        int mid = si + (ei - si)/2; //(si+ei)/2
+        mergesort(arr, si, mid);    //left part
+        mergesort(arr, mid+1, ei);   //right part
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int arr[], int si, int mid, int ei){
+        int temp[] = new int[ei-si+1];
+        int i = si;  //iterator for left part
+        int j = mid+1; //iterator for right part
+        int k = 0;   //iterator for temp arr
+        while(i<= mid && j<= ei){
+            if(arr[i]<arr[j]){
+                temp[k++] = arr[i++];
+                
+            } else{
+                temp[k++] = arr[j++];
+        
+            }
+           
+        }
+        //left part
+        while(i<= mid){
+            temp[k++] = arr[i++];
+
+        }
+        //right part
+        while(j<= ei){
+            temp[k++] = arr[j++];
+
+        }
+        //copy to my original array
+        for(k=0, i=si; k<temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array :");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter array elements : ");
+        for (int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+
+        }   
+        System.out.println("original array : ");
+        divideconquer(arr);
+        mergesort(arr, 0, arr.length-1);
+        System.out.println("sorted array : ");
+        divideconquer(arr);
+        sc.close();
+
+        }
+
+    
+    
+}
