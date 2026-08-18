71776fd62ad2a1df4ac056b65e4e9bb9d09def92
Manish Prasad <manishprasad.iot2024@nsec.ac.in>
2026-08-18 08:32:55 +0530

    Add findSUBSETS.java to implement a recursive algorithm for finding all subsets of a string

---
+++ DSA JAVA/RECURSION/findSUBSETS.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class findSUBSETS {
+    public static void findsubsets(String str, String ans, int i){
+        //base case
+        if(i==str.length()){
+            System.out.println(ans);
+            return;
+        }
+        //yes choice
+        findsubsets(str, ans+str.charAt(i), i+1);
+        //no choice
+        findsubsets(str, ans, i+1);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string : ");
+        String str = sc.next();
+        findsubsets(str, "", 0);
+        sc.close();
+
+
+    }
+    
+}
